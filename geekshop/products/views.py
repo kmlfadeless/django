9c5fd3e8bef9ee94bb6fcd155899fd9bb09900b6
Kostiantyn Lozhkin <k@kml.email>
2018-10-26 12:56:43 +0300

    DZ3 - draft

--- geekshop/products/views.py
+++ geekshop/products/views.py
@@ -1,26 +1,22 @@
 import json
 from django.shortcuts import render
 
-
-def product_list(request):
-    context = {}
-
-    with open('products/data/products.json', 'r') as file:
-        context = json.load(file)
-
-    return render(request, 'products/catalog.html', context)
+from .models import Product, Category
 
 
-def product_detail(request, idx):
-    context = {}
+def category_list(request):
+    query = Category.objects.all()
 
-    with open('products/data/products.json', 'r') as file:
-        context = json.load(file)
+    return render(request, 'products/catalog.html', {'categories': query})
 
-    return render(
-        request,
-        'products/detail.html',
-        {
-            'product': context['products'][idx]
-        }
-    )
+
+def product_list(request, pk):
+    query = Product.objects.filter(category=pk)
+
+    return render(request, 'products/category.html', {'products': query})
+
+
+def product_detail_by_url_key(request, url_key):
+    obj = Product.objects.get(url_key=url_key)
+
+    return render(request, 'products/detail.html', {'object': obj})
