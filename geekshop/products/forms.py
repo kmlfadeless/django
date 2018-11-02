9cce6072127809de605c5e451da7a467ff057d11
Kostiantyn Lozhkin <k@kml.email>
2018-11-02 03:05:31 +0100

    DZ5

---
+++ geekshop/products/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+
+from .models import Product
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = Product
+        fields = [
+            'title', 'alt', 'name',
+            'description', 'characteristics', 'details',
+            'url_key', 'category'
+        ]
